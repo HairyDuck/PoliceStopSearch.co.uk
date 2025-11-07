a5221fd45e821f05447256ddd1ca890718ec4291
Luke <lukeaddington@gmail.com>
2025-11-07 11:41:54 +0000

    Enhances social media links and icons
    Improves the appearance and functionality of social media engagement methods by:
    
    - Hides generic web links from the engagement methods
    - Adds support for "X" (Twitter) and Flickr
    - Uses SVG icons instead of text for better visual representation
    - Introduces a label to clarify some social media links

--- pages/forces.vue
+++ pages/forces.vue
@@ -113,7 +113,7 @@
               <div class="grid grid-cols-1 md:grid-cols-2 gap-3">
                 <template v-for="method in forceDetails[force.id].engagement_methods" :key="method.type">
                   <a
-                    v-if="method.url && method.type !== 'telephone'"
+                    v-if="method.url && method.type !== 'telephone' && method.type !== 'web'"
                     :href="method.url"
                     target="_blank"
                     rel="noopener noreferrer"
@@ -121,9 +121,9 @@
                     :class="getSocialMediaClass(method.type)"
                   >
                     <div class="flex items-center">
-                      <span class="text-2xl mr-3">{{ getSocialMediaIcon(method.type) }}</span>
+                      <div class="w-6 h-6 mr-3 flex-shrink-0" v-html="getSocialMediaIcon(method.type)"></div>
                       <div>
-                        <span class="font-medium">{{ method.title || method.type }}</span>
+                        <span class="font-medium">{{ getSocialMediaLabel(method.type) || method.title || method.type }}</span>
                         <div v-if="method.description" class="text-sm opacity-75" v-html="sanitizeHtml(method.description)"></div>
                       </div>
                     </div>
@@ -470,17 +470,21 @@ const getForceNameById = (forceId: string | null) => {
 const getSocialMediaClass = (type: string) => {
   if (!type) return 'bg-gray-50 text-gray-700 hover:bg-gray-100'
   
-  switch (type.toLowerCase()) {
+  const lowerType = type.toLowerCase()
+  switch (lowerType) {
     case 'facebook':
       return 'bg-blue-50 text-blue-700 hover:bg-blue-100'
     case 'twitter':
-      return 'bg-sky-50 text-sky-700 hover:bg-sky-100'
+    case 'x':
+      return 'bg-black text-white hover:bg-gray-800'
     case 'youtube':
       return 'bg-red-50 text-red-700 hover:bg-red-100'
     case 'instagram':
       return 'bg-pink-50 text-pink-700 hover:bg-pink-100'
     case 'linkedin':
       return 'bg-blue-50 text-blue-700 hover:bg-blue-100'
+    case 'flickr':
+      return 'bg-pink-50 text-pink-700 hover:bg-pink-100'
     case 'rss':
       return 'bg-orange-50 text-orange-700 hover:bg-orange-100'
     default:
@@ -488,24 +492,49 @@ const getSocialMediaClass = (type: string) => {
   }
 }
 
-const getSocialMediaIcon = (type: string) => {
-  if (!type) return '🔗'
+const getSocialMediaIcon = (type: string): string => {
+  if (!type) {
+    return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"/></svg>'
+  }
   
-  switch (type.toLowerCase()) {
+  const lowerType = type.toLowerCase()
+  switch (lowerType) {
     case 'facebook':
-      return '📘'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>'
     case 'twitter':
-      return '🐦'
+    case 'x':
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>'
     case 'youtube':
-      return '📺'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"/></svg>'
     case 'instagram':
-      return '📸'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/></svg>'
     case 'linkedin':
-      return '💼'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>'
+    case 'flickr':
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M7.5 16c-2.206 0-4-1.794-4-4s1.794-4 4-4 4 1.794 4 4-1.794 4-4 4zm0-6c-1.103 0-2 .897-2 2s.897 2 2 2 2-.897 2-2-.897-2-2-2zm9-2c-2.206 0-4 1.794-4 4s1.794 4 4 4 4-1.794 4-4-1.794-4-4-4zm0 6c-1.103 0-2-.897-2-2s.897-2 2-2 2 .897 2 2-.897 2-2 2z"/></svg>'
     case 'rss':
-      return '📰'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M6.503 20.752c0 1.794-1.456 3.248-3.251 3.248-1.796 0-3.252-1.454-3.252-3.248 0-1.794 1.456-3.248 3.252-3.248 1.795.001 3.251 1.454 3.251 3.248zm-6.503-12.572v4.811c6.05.062 10.96 4.966 11.022 11.009h4.817c-.062-8.71-7.118-15.758-15.839-15.82zm0-3.368c10.58.046 19.152 8.594 19.183 19.188h4.817c-.03-13.231-10.755-23.954-24-24v4.812z"/></svg>'
     default:
-      return '🔗'
+      return '<svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24"><path d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"/></svg>'
+  }
+}
+
+const getSocialMediaLabel = (type: string): string => {
+  if (!type) return ''
+  
+  const lowerType = type.toLowerCase()
+  switch (lowerType) {
+    case 'twitter':
+    case 'x':
+      return 'X (Twitter)'
+    case 'facebook':
+      return 'Facebook'
+    case 'flickr':
+      return 'Flickr'
+    case 'youtube':
+      return 'YouTube'
+    default:
+      return ''
   }
 }
 
